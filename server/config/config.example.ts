455a197acc57837fbee9518f32ba9151cf3e8b8a
Tiyashi Gunawardana <155960783+TiyashiKavindya@users.noreply.github.com>
2024-03-15 22:41:09 +0530

    db config example file

---
+++ server/config/config.example.ts
@@ -0,0 +1,9 @@
+// express server configuration
+export const SERVER_PORT = 5400
+
+// MySQL server configuration
+export const DATABASE_HOST = "Database host"
+export const DATABASE_NAME = "Database name"
+export const DATABASE_USER = "Database user"
+export const DATABASE_PASSWORD = "Database password"
+export const DATABASE_PORT = 3306
