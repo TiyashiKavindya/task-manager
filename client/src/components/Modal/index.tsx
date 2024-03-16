9d7631e1a715471f8c7698088fe2060ad65cdf7d
Tiyashi Gunawardana <155960783+TiyashiKavindya@users.noreply.github.com>
2024-03-16 22:58:48 +0530

    overflow scroll

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -12,7 +12,7 @@ function Modal({ children, title, className }: ModalProps) {
     return (
         <dialog open={showModal} className={className}>
             <div className="fixed z-[9998] inset-0 bg-black bg-opacity-70"></div>
-            <div className="fixed md:top-1/2 bottom-2 left-1/2 transform -translate-x-1/2 md:-translate-y-1/2 bg-white z-[9999] px-4 py-6 md:px-6 rounded-xl shadow-lg min-w-[95%] mx-auto md:min-w-[60%] lg:min-w-[40%] min-h-56 h-fit">
+            <div className="fixed md:top-1/2 bottom-2 left-1/2 transform -translate-x-1/2 md:-translate-y-1/2 bg-white z-[9999] px-4 py-6 md:px-6 rounded-xl shadow-lg min-w-[95%] mx-auto md:min-w-[60%] lg:min-w-[40%] min-h-56 h-fit max-h-[95dvh] overflow-y-auto">
                 <p className='text-lg font-semibold mb-2'>{title}</p>
                 {children}
             </div>
