f31a8dd9c083f439cefe9a8d8a7e104c93d62d74
Tiyashi Gunawardana <155960783+TiyashiKavindya@users.noreply.github.com>
2024-03-21 10:08:39 +0530

    toast hide

--- client/src/Toast/index.tsx
+++ client/src/Toast/index.tsx
@@ -3,7 +3,7 @@ import { useAppContext } from "../contexts"
 function Toast() {
     const { showToast, toastMsg } = useAppContext()
     return (
-        <div className={`fixed bottom-10 px-6 bg-white py-4 rounded-lg shadow-lg border min-w-80 border-emerald-600 duration-300 ease-in-out ${showToast ? ' right-10 opacity-100' : ' right-0 opacity-0'}`}>
+        <div className={`fixed bottom-10 px-6 bg-white py-4 rounded-lg shadow-lg border min-w-80 border-emerald-600 cursor-wait duration-300 ease-in-out ${showToast ? ' right-10 opacity-100' : ' -right-96 opacity-0'}`}>
             <p>{toastMsg.title}</p>
             <p className="text-gray-600 text-xs font-thin">{toastMsg.message}</p>
         </div>
