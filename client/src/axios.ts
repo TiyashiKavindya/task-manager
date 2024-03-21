d8216d19212804a5bec2a9b956dc5cffb684d02c
Tiyashi Gunawardana <155960783+TiyashiKavindya@users.noreply.github.com>
2024-03-21 10:08:49 +0530

    remove timeout

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -2,7 +2,6 @@ import Axios from 'axios'
 
 const axios = Axios.create({
     baseURL: 'http://localhost:5400/api',
-    timeout: 3000,
     headers: {
         'Content-Type': 'application/json'
     }
