1225eaf7027a852a5cfac0b06012818c0da37485
Tiyashi Gunawardana <155960783+TiyashiKavindya@users.noreply.github.com>
2024-04-07 14:54:53 +0530

    ngrok server

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -1,7 +1,8 @@
 import Axios from 'axios'
+import { API_URL } from './config'
 
 const axios = Axios.create({
-    baseURL: 'http://localhost:5400/api',
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:5400/api' : API_URL,
     headers: {
         'Content-Type': 'application/json'
     }
---
+++ client/src/config.ts
@@ -0,0 +1,1 @@
+export const API_URL = 'https://70b9-212-104-228-20.ngrok-free.app/api'
